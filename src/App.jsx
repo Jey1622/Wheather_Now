94bff5305e9e8854430d90533d6d30027e729e09
Jeyaraman S <jeyaramanapr22@gmail.com>
2025-10-27 19:53:14 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is working! 🎉
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
